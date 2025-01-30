f85888ad10dd2dd3f3a682f644710fbec9d6db51
luiscampoy93 <luis_campoy18@hotmail.com>
2025-01-30 03:52:00 +0100

    Add files via upload

---
+++ Ejecicio1B.js
@@ -0,0 +1,9 @@
+
+const cumpleanos = {
+    dia:  30,
+    mes: 'Julio'
+}
+
+console.log('Mi cumpleaños es ' + cumpleanos.dia + ' de ' +  cumpleanos.mes)
+
+console.log(`Mi cumpleaños es el ${cumpleanos.dia} de ${cumpleanos.mes}`)
---
+++ Ejercicio1A.js
@@ -0,0 +1,13 @@
+
+// Utilización de variables
+
+const lado1 = 34
+const lado2 = 23
+const altura = 13
+
+const volumenCubo = lado1 * lado2 *altura
+
+console.log(volumenCubo)
+
+
+
